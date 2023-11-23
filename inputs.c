c20670d3f74d348baa6cd4d3102362d820f80ef1
maxaik <maxolof1@hotmail.com>
2023-11-23 14:39:18 +0100

    optimerad

--- inputs.c
+++ inputs.c
@@ -1,46 +1,27 @@
 #include <pic32mx.h>
 
-int get_button_1() {
-    int data = PORTF;
-    data = data >> 1;
+/* Hardcoded port and shift number for each button */
+int get_button(int port, int shift) {
+    int data = port;
+    data = data >> shift;
     data &= 0b1;
     return data;
 }
 
-int get_button_2() {
-    int data = PORTD;
-    data = data >> 5;
-    data &= 0b1;
-    return data;
-}
-
-int get_button_3() {
-    int data = PORTD;
-    data = data >> 6;
-    data &= 0b1;
-    return data;
-}
-
-int get_button_4() {
-    int data = PORTD;
-    data = data >> 7;
-    data &= 0b1;
-    return data;
-}
-
+/* Check if "button" is pressed */
 int get_buttons(int button){
     switch(button) {
         case 1:
-            return get_button_1();
+            return get_button(PORTF, 1);
         break;
         case 2:
-            return get_button_2();
+            return get_button(PORTD, 5);
         break;
         case 3:
-            return get_button_3();
+            return get_button(PORTD, 6);
         break;
         case 4:
-            return get_button_4();
+            return get_button(PORTD, 7);
         break;
 }
     
