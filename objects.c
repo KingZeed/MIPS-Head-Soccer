2e8469f7fd5042c5b9a5a41cd5db67651bcb679f
Mans Zellman <mans.zellman@gmail.com>
2023-11-24 11:48:14 +0100

    Add structs for ball, players

--- objects.c
+++ objects.c
@@ -1,3 +1,22 @@
 #include <pic32mx.h>
 #include <stdint.h>
 #include "project.h"
+
+struct Ball {
+    int pos_x;
+    int pos_y;
+    int speed;
+    int acceleration;
+};
+
+struct Player1 {
+    int pos_x;
+    int pos_y;
+    int speed;
+};
+
+struct Player2 {
+    int pos_x;
+    int pos_y;
+    int speed;
+};
