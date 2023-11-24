f8d7a422bb6e8df6da7273c36ecf8ef7c63595c6
Mans Zellman <mans.zellman@gmail.com>
2023-11-24 14:22:33 +0100

    Add structs in header file

--- project.h
+++ project.h
@@ -1,5 +1,22 @@
 uint8_t display[32][128]; // pixel coordinate system
 
+struct Ball {
+    int pos_x;
+    int pos_y;
+    int speed;
+    int acceleration;
+};
+
+struct Player {
+    int pos_x;
+    int pos_y;
+    int speed;
+};
+
+struct Ball ball = {0, 0, 0, 0};
+struct Player p1 = {0, 0, 0};
+struct Player p2 = {0, 0, 0};
+
 void display_init();
 
 void display_clear();
@@ -18,13 +35,13 @@ void draw_left_goal();
 
 void draw_right_goal();
 
-void draw_ball();
+void draw_ball(int x, int y);
 
-void draw_player1();
+void draw_player1(int x, int y);
 
-void draw_player2();
+void draw_player2(int x, int y);
 
-int get_button(int button_numbe);
+int get_button(int button_number);
 
 int get_switchtoggle(int switch_number);
 
